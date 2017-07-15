260ec3f75558884e87deda4eface1e1a06562a87
AndriiShostatskyi <shostatscky.andriy@yandex.ua>
2017-07-15 19:12:47 +0300

    asd

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,8 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^', include('news.urls', namespace="mynews")),
     url(r'^news', include('news.urls', namespace="mynews")),
-    url(r'^admin', include('myadmin.urls')),
-    url(r'^admin/tinymce/', include('tinymce.urls')),
 
     #url(r'^admin/', admin.site.urls),
 ]
